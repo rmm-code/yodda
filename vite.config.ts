5ea09c373a16c68e2a19691f6abd32898cc45c64
rmm-code <rmmstudy369@gmail.com>
2025-11-27 23:35:03 +0500

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: [
-      'yodda.app',
-      'www.yodda.app',
-      'localhost',
-      '127.0.0.1',
-    ],
+    allowedHosts: true,
   },
   preview: {
     host: true,
     port: 4173,
-    allowedHosts: [
-      'yodda.app',
-      'www.yodda.app'
-    ],
+    allowedHosts: true,
   },
 })
