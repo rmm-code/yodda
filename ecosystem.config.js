b82ad88acb86b40f215d265946a1b1f5bea82d29
rmm-code <rmmstudy369@gmail.com>
2025-11-27 23:01:37 +0500

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,37 @@
 module.exports = {
-  apps: [{
-    name: 'yodda-bot',
-    script: 'bot.py',
-    interpreter: 'venv/bin/python3',
-    cwd: '/var/www/yodda-bot',
-    env: {
-      NODE_ENV: 'production'
+  apps: [
+    {
+      name: 'yodda-bot',
+      script: 'bot.py',
+      interpreter: 'venv/bin/python3',
+      cwd: '/var/www/yodda-bot',
+      env: {
+        NODE_ENV: 'production'
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
     },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-    merge_logs: true,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-  }]
+    {
+      name: 'yodda-preview',
+      script: 'npm',
+      args: 'run preview',
+      cwd: process.cwd(),
+      env: {
+        NODE_ENV: 'production'
+      },
+      error_file: './logs/preview-err.log',
+      out_file: './logs/preview-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+    }
+  ]
 };
 
